3fef1fb967fc792c4f1655de9e663d2841bf5b14
Chris <chriswong90012@yahoo.com>
2021-07-20 19:27:54 -0700

    adding bootstrap

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import './Products.css'
+import React from "react";
+import "./Products.css";
 
 export default class Products extends React.Component {
   render() {
-    return (
-      <div className="products-button"></div>
-    );
+    return <div className="container"></div>;
   }
 }
