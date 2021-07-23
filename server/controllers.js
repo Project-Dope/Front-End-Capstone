aa1486a614d51df2f647559339803c2f9404b300
Jean-Luc <jlfelix522@yahoo.com>
2021-07-23 16:15:33 -0700

    Changed API key

--- server/controllers.js
+++ server/controllers.js
@@ -1,17 +1,17 @@
-// var axios = require('axios');
+var axios = require('axios');
 
 var config = {
   method: 'get',
   url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products',
   headers: {
-    'Authorization': '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'
+    'Authorization': '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'
   }
 };
 
-// axios(config)
-// .then(function (response) {
-//   console.log(JSON.stringify(response.data));
-// })
-// .catch(function (error) {
-//   console.log(error);
-// });
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
