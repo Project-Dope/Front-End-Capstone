485937dec4844536de4c297a8abf9c36ec5dadb6
Jean-Luc <jlfelix522@yahoo.com>
2021-07-23 18:28:30 -0700

    Added new token in config.js

--- server/controllers.js
+++ server/controllers.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-var axios = require('axios');
-
-var config = {
-  method: 'get',
-  url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products',
-  headers: {
-    'Authorization': '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'
-  }
-};
-
-axios(config)
-.then(function (response) {
-  console.log(JSON.stringify(response.data));
-})
-.catch(function (error) {
-  console.log(error);
-});
-=======
 const axios = require("axios");
 const config = require("./config/config.js");
 
@@ -96,4 +77,3 @@ module.exports = {
     },
   },
 };
->>>>>>> main
