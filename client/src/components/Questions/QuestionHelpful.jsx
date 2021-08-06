0900722849f1aa427bba56cbba07f46651552bd8
Ryan Chi <ryanchi9170@gmail.com>
2021-08-06 11:47:27 -0700

    debugged merge conflicts, page renders but some of jean's components are not up to date

--- client/src/components/Questions/QuestionHelpful.jsx
+++ client/src/components/Questions/QuestionHelpful.jsx
@@ -38,21 +38,6 @@ class QuestionHelpful extends React.Component {
     )
   }
 
-// var QuestionHelpful = (props) => {
-//   const answerModal = function(event) {
-//     event.preventDefault();
-//     props.setModalType('add answer')
-//     props.openModal(props.questionId);
-//   }
-
-//   return (
-//     <span>
-//       <span>Helpful?&nbsp;
-//         <button className='questions-helpful-buttons' onClick={() => props.addQuestionHelpfulness()}>Yes</button>
-//         <span> {props.questionHelpfulness}  |  <button className='questions-helpful-buttons' onClick={answerModal}>
-//           Add Answer</button></span>
-//       </span>
-//     </span>
-//   )
+}
 
 export default QuestionHelpful;
