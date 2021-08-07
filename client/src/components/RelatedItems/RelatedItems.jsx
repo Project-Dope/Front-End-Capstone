98d1261c93f5ba7f67e3aa9caedde6e8ffe1992f
Chris <chriswong90012@yahoo.com>
2021-08-06 23:28:53 -0600

    adding to czwong

--- client/src/components/RelatedItems/RelatedItems.jsx
+++ client/src/components/RelatedItems/RelatedItems.jsx
@@ -13,6 +13,7 @@ export default class RelatedItems extends React.Component {
       currentID: null,
       relatedItems: null,
       showComparisonModal: false,
+      asdas: false,
     };
 
     this.openComparisonModal = this.openComparisonModal.bind(this);
